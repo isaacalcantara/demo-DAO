e34ac04d899ad1a39e82529356b8f95475bec17c
Isaac Alcantara <isaacalcz1@gmail.com>
2022-07-04 16:13:12 -0300

    findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,17 @@
 package application;
 
-import java.util.Date;
-
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
-		System.out.println(obj);
-		System.out.println(seller);
 		
+		SellerDao sellerdao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerdao.findById(3);
+		System.out.println(seller);
 		
 	}
 
